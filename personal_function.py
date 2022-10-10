18a3a332921dfaa54293dc1c8ea275930a26e452
Giang Truong Nguyen <74754581+giangnguyenquantum@users.noreply.github.com>
2022-10-10 19:35:27 +0800

    Update personal_function.py

--- personal_function.py
+++ personal_function.py
@@ -5,8 +5,8 @@ from matplotlib.pyplot import hist
 import pandas as pd
 
 def client():
-    apikey='MZoUNKZin6hLFAaPtMyi0GrZXjz8ca55OL4Qlmys3YuUOSzS6fdtW97WvlKzs5yA'
-    secret='QuIoCo9pTkb8hw1ktIXZoxtPKnG9e2kXYCLy05c876aHqA4NL1T4s5ZAWtD2q9y4'
+    apikey=''
+    secret=''
     return Client(apikey, secret)
 
 
